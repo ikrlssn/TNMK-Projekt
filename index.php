e136b2c9bd1c51537c03accecddbf02406694cfe
ikrlssn <isakkarlsson0@gmail.com>
2021-11-29 12:59:28 +0100

    ny kod index&style

--- index.php
+++ index.php
@@ -1,27 +1,35 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8"/>
-    <link rel="stylesheet" href="style.css">
-    <title>Home</title>
+	<meta charset="utf-8">
+	<title>Lego Search</title>
+	<link rel="stylesheet" href="style.css">
+	<script src="legoscript.js"></script>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=M+PLUS+1+Code&family=Quantico&family=Rajdhani:wght@600&display=swap" rel="stylesheet">
+	<link rel="icon" type="image/png" href="legohead.png">
 </head>
 
 <body>
-
-    <div class="header">
-        <p>här kommer en meny</p>
-    </div>
-    <div class="wrapper">
-        <p> allt skit </p>
-        <div id="searchmain">
-            <form method="get">
-                <!-- autofocus väljer input-fältet direkt när sidan laddas så man kan skriva in direkt -->
-                <input type="text" name="search" placeholder="Search lego-set" autofocus>
-                <input type="submit" value="Search">
-            </form>
-        </div>
-    </div>
-
+	<div class="header">
+		<img src="logo.svg" alt="logo" id="logo">
+		<h3><a href="">About</a></h3>
+	</div>
+	<div class="wrapper">
+		<div id="help">
+			<img src="legoman.png" alt="legoman" id="legoman">
+			<h3 onClick="showhelp()"><a>Help</a></h3>
+		</div>
+		<div id="search">
+			<h1>Lego Search</h1>
+			<input id="searchbar" type="text" autofocus placeholder="Search for LEGO sets here...">
+			<button type="submit">Search</button>
+		</div>
+		<div id="helpcontent">
+			<p>According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.</p>
+		</div>
+		
+	</div>
 </body>
-
 </html>
