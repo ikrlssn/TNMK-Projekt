0de5844d129f724edb9eea262bf0f2b0e20b0dcd
ikrlssn <isakkarlsson0@gmail.com>
2021-11-30 11:26:45 +0100

    javascript tillagt

--- legoscript.js
+++ legoscript.js
@@ -1,1 +1,12 @@
 
+function showhelp() {
+	
+	var div = document.getElementById("helpcontent");
+	
+	if (div.style.display === "block"){
+		div.style.display = "none";
+	} 
+	else {
+		div.style.display = "block";
+	}
+}
